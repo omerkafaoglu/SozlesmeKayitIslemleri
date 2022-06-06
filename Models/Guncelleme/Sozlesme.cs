a8332ac91e2be87c7b16724e705764c8b3e326bb
Ömer Kafaoğlu <omer.kafaoglu@OMERKAFAOGLU>
2022-06-06 17:53:32 +0300

    Proje dosyası ekle.

---
+++ Models/Guncelleme/Sozlesme.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace SozlesmeKayitIslemleri.Models.Guncelleme
+{
+    public class Sozlesme
+    {
+        [Key]
+        [MaxLength(500)]
+        public string SicilNumarasi { get; set; }
+        public string TCVergiKimlikNo { get; set; }
+        public string UnvanAdSoyad { get; set; }
+        public DateTime BaslangicTarihi { get; set; }
+
+        public DateTime BitisTarihi { get; set; }
+        public string[] Durumu = { "Açık", "Normal Yürüyor", "Onayda", "Kapalı" };
+        public string[] SozlesmeTuru = { "Kamu", "Özel" };
+        public string Aciklama { get; set; }
+        public string TalepNumarasi { get; set; }  
+        public DateTime GelenEvrakTarihi { get; set; }
+        public int GelenEvrakSayisi { get; set; }
+        public DateTime OlurTarihi { get; set; }
+        public int OlurSayisi { get; set; }
+        
+
+
+
+
+    }
+}
---
+++ Veri/SozlesmeDBContext.cs
@@ -0,0 +1,18 @@
+﻿using SozlesmeKayitIslemleri.Models.Guncelleme;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SozlesmeKayitIslemleri.Models
+{
+    public class SozlesmeDBContext:DbContext
+    {
+        public SozlesmeDBContext():base("name=BaglantiCumlesi")
+        {
+
+        }
+        public DbSet<Sozlesme>  Sozlesme { get; set; }
+    }
+}
